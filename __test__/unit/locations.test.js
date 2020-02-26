96baf0b2ea11b282da4837390cf2bcde11a584cd
luukkonen1991 <luukkonen1991@gmail.com>
2020-02-26 15:55:37 +0200

    tests

--- __test__/unit/locations.test.js
+++ __test__/unit/locations.test.js
@@ -6,7 +6,7 @@ const newLocation = require('../mockData/new-location.json');
 jest.mock('../../models/Location');
 
 let req, res, next;
-let locationId = '5d713995b721c3bb35c1f5d0';
+let locationId = '5d713995b721c3bb38c1f5d0';
 beforeEach(() => {
   req = httpMocks.createRequest();
   res = httpMocks.createResponse();
@@ -16,6 +16,13 @@ beforeEach(() => {
 // Unit test for GET locations
 describe('locations.getLocations', () => {
   it('should have a getLocations function', () => {
-    expect(typeof locations.getLocations).toBe('function')
+    expect(typeof locations.getLocations).toBe('function');
+  });
+  it('should call findLocations', async () => {
+    await locations.getLocations(req, res, next);
+    expect(locations.getLocations).toHaveBeenCalledWith();
+  });
+  it('should return a response 200 and locations', async () => {
+    location.getLocations
   });
 });
