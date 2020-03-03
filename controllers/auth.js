f5f2e6706574432f217517bd796d392d96b3bb15
luukkonen1991 <luukkonen1991@gmail.com>
2020-03-03 16:25:14 +0200

    typo in ErrorResponses

--- controllers/auth.js
+++ controllers/auth.js
@@ -37,7 +37,7 @@ exports.login = asyncHandler(async (req, res, next) => {
 
   // Validate email & password
   if (!email || !password) {
-    return next(new ErrorResponse('Please provide an email and password'), 400);
+    return next(new ErrorResponse('Please provide an email and password', 400));
   };
 
   //Check for user
@@ -45,13 +45,13 @@ exports.login = asyncHandler(async (req, res, next) => {
     email: email
   }).select('+password');
   if (!user) {
-    return next(new ErrorResponse('Invalid credentials'), 401);
+    return next(new ErrorResponse('Invalid credentials', 401));
   }
 
   // Check if password matches
   const isMatch = await user.matchPassword(password);
   if (!isMatch) {
-    return next(new ErrorResponse('Invalid credentials'), 401);
+    return next(new ErrorResponse('Invalid credentials', 401));
   }
 
   sendTokenResponse(user, 200, res);
