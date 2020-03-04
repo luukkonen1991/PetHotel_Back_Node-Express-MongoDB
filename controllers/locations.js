4ffd31b0ecaccd66eb692b795d262256d328bf9a
luukkonen1991 <luukkonen1991@gmail.com>
2020-03-04 11:57:04 +0200

    added checking location owner for uploadPhoto also

--- controllers/locations.js
+++ controllers/locations.js
@@ -84,6 +84,11 @@ exports.deleteLocation = asyncHandler(async (req, res, next) => {
     return next(new ErrorResponse(`Location not found with id of ${req.params.id}`, 404));
   }
 
+  // Make sure user is location owner
+  if (location.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(new ErrorResponse(`User ${req.user.id} is not authorized to delete this location`, 401));
+  }
+
   location.remove();
 
   res.status(200).json({
@@ -100,6 +105,11 @@ exports.locationPhotoUpload = asyncHandler(async (req, res, next) => {
 
   if (!location) {
     return next(new ErrorResponse(`Location not found with id of ${req.params.id}`, 404));
+  }
+
+  // Make sure user is location owner
+  if (location.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(new ErrorResponse(`User ${req.user.id} is not authorized to update this location`, 401));
   }
 
   if (!req.files) {
