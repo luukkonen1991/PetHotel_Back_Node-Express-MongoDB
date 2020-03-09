0947a13db5f4e32a97e18d6f2eaaf9fbe821d1b9
luukkonen1991 <luukkonen1991@gmail.com>
2020-03-09 10:32:07 +0200

    forgotPassword route added and testet

--- models/User.js
+++ models/User.js
@@ -52,5 +52,20 @@ UserSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
+//Generate and hash password token
+UserSchema.methods.getResetPasswordToken = function () {
+  const resetToken = crypto.randomBytes(20).toString('hex');
+
+  // Hash token and set to resetPasswordToken field
+  this.resetPasswordToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+
+  // Set expire
+  this.resetPasswordExpire = Date.now() + 10 * 60 * 1000; //10 mins
+
+  return resetToken;
+};
 
 module.exports = mongoose.model('User', UserSchema);
--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,8 @@ const express = require('express');
 const {
   register,
   login,
-  getMe
+  getMe,
+  forgotPassword
 } = require('../controllers/auth');
 
 const router = express.Router();
@@ -12,6 +13,7 @@ const { protect } = require('../middleware/auth');
 router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protect, getMe);
+router.post('/forgotpassword', forgotPassword);
 
 
 module.exports = router;
