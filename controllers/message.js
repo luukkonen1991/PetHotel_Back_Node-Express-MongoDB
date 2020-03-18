5e8a79bb9ab574d16234202e033261ee75c2da4a
luukkonen1991 <luukkonen1991@gmail.com>
2020-03-18 14:43:03 +0200

    added mockdata for messages and started message.controller

---
+++ controllers/message.js
@@ -0,0 +1,10 @@
+const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middleware/async');
+const Message = require('../models/Message');
+
+//@desc       Get all messages
+//@route      GET /api/v1/messages
+//@access     Private/Admin
+exports.getMessages = asyncHandler(async (req, res, next) => {
+  res.status(200).json(res.advancedResults);
+});
