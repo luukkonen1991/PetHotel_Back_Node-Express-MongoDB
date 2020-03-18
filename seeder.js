854b7bc76417494d6aa962a1f7c03879634e9a56
luukkonen1991 <luukkonen1991@gmail.com>
2020-03-18 14:45:32 +0200

    added messages mockdata to seeder

--- seeder.js
+++ seeder.js
@@ -11,6 +11,7 @@ dotenv.config({
 // Load models
 const Location = require('./models/Location');
 const User = require('./models/User');
+const Message = require('./models/Message');
 
 
 // Connect to DB
@@ -23,31 +24,36 @@ mongoose.connect(process.env.MONGO_URI, {
 
 // Read JSON files
 const locations = JSON.parse(fs.readFileSync(`${__dirname}/_data/locations.json`, 'utf-8'));
+
 const users = JSON.parse(fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8'));
+
+const messages = JSON.parse(fs.readFileSync(`${__dirname}/_data/messages.json`, 'utf-8'));
 
 // Import into DB
 const importData = async () => {
   try {
     await Location.create(locations);
     await User.create(users);
+    await Message.create(messages);
     console.log('Data imported...'.green.inverse);
     process.exit();
   } catch (error) {
     console.error(error);
   }
-}
+};
 
 // Delete data in db
 const deleteData = async () => {
   try {
     await Location.deleteMany();
     await User.deleteMany();
+    await Message.deleteMany();
     console.log('Data destroyed...'.red.inverse);
     process.exit();
   } catch (error) {
     console.error(error);
   }
-}
+};
 
 if (process.argv[2] === '-i') {
   importData();
