8a1008d72125600200a4c3543c729033e97a5c1b
luukkonen1991 <luukkonen1991@gmail.com>
2020-03-20 14:04:42 +0200

    added geoCoder for location and google maps

--- models/Location.js
+++ models/Location.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const geocoder = require('../utils/geocoder');
 
 const LocationSchema = new mongoose.Schema({
   title: {
@@ -47,6 +48,15 @@ const LocationSchema = new mongoose.Schema({
     required: [true, 'Please add the cost amount']
   },
   location: {
+    //GeoJSON Point
+    type: {
+      type: String,
+      enum: ['Point']
+    },
+    coordinates: {
+      type: [Number],
+      index: '2dsphere'
+    },
     formattedAddress: String,
     street: String,
     city: String,
@@ -98,5 +108,24 @@ LocationSchema.pre('save', function (next) {
   next();
 });
 
+// Geocode & create location field
+LocationSchema.pre('save', async function (next) {
+  const loc = await geocoder.geocode(this.address);
+  this.location = {
+    type: 'Point',
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+    street: loc[0].streetName,
+    city: loc[0].city,
+    state: loc[0].stateCode,
+    zipcode: loc[0].zipcode,
+    country: loc[0].countryCode
+  };
+
+  // Do not save addres in DB
+  // this.address = undefined
+
+  next();
+});
 
 module.exports = mongoose.model('Location', LocationSchema);
