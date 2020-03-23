a7ebee1d614e975b75681d3e46c4b9c9f3996a6e
luukkonen1991 <luukkonen1991@gmail.com>
2020-03-23 15:03:31 +0200

    CRUD ready on reviews

--- routes/reviews.js
+++ routes/reviews.js
@@ -2,7 +2,9 @@ const express = require('express');
 const {
   getReviews,
   getReview,
-  addReview
+  addReview,
+  updateReview,
+  deleteReview
 } = require('../controllers/reviews');
 
 const Review = require('../models/Review');
@@ -25,6 +27,8 @@ router
 
 router
   .route('/:id')
-  .get(getReview);
+  .get(getReview)
+  .put(protect, authorize('user', 'admin'), updateReview)
+  .delete(protect, authorize('user', 'admin'), deleteReview);
 
 module.exports = router;
