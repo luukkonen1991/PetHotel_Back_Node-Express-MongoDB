994a79416e6493a965d8fb5d8ceda2f3a0481976
luukkonen1991 <luukkonen1991@gmail.com>
2020-03-30 14:23:24 +0300

    rateLimiter and prevent http param pollution

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const cors = require('cors');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 
@@ -49,6 +51,17 @@ app.use(helmet());
 
 // Prevent XSS attacks
 app.use(xss());
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 mins
+  max: 100
+});
+
+app.use(limiter);
+
+// Prevent http param pollution
+app.use(hpp());
 
 // Enable CORS
 app.use(cors());
