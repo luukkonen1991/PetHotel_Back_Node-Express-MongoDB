75db3f22f4dcffbc8c244660852eff071e462793
luukkonen1991 <luukkonen1991@gmail.com>
2020-04-07 15:45:52 +0300

    users/publishers able to delete their own userinfo/profile

--- routes/users.js
+++ routes/users.js
@@ -14,20 +14,18 @@ const router = express.Router();
 const advancedResults = require('../middleware/advancedResults');
 const { protect, authorize } = require('../middleware/auth');
 
-// Anything below this statement will use protect middleware
-router.use(protect);
-// Anything below this statement will use authorize('admin') middleware
-router.use(authorize('admin'));
+// // Anything below this statement will use protect middleware
+// router.use(protect);
+// // Anything below this statement will use authorize('admin') middleware
+// router.use(authorize('admin'));
 
-router
-  .route('/')
-  .get(advancedResults(User), getUsers)
-  .post(createUser);
+router.route('/')
+  .get(protect, authorize('admin'), advancedResults(User), getUsers)
+  .post(protect, createUser);
 
-router
-  .route('/:id')
-  .get(getUser)
-  .put(updateUser)
-  .delete(deleteUser);
+router.route('/:id')
+  .get(protect, getUser)
+  .put(protect, updateUser)
+  .delete(protect, deleteUser);
 
 module.exports = router;
